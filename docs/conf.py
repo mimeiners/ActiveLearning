635e367b8e777aa7ef0a893a33666c478b42e65d
Mirco Meiners <Mirco.Meiners@hs-bremen.de>
2024-10-21 13:10:06 +0200

    Error fix

--- docs/conf.py
+++ docs/conf.py
@@ -68,9 +68,9 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-#language = u'de'
-#locale_dirs = ['locale/']
-#gettext_compact = False
+language = u'de'
+locale_dirs = ['locale/']
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -204,7 +204,7 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # -- Options for todo extension ----------------------------------------------
 
